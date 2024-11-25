455de2fb172adde481cbcbe6bff8252f45dd05d1
Lonnie Golen <lgolen@css.edu>
2024-11-24 18:43:05 -0600

    Changes to fix compile errors

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,20 @@
-﻿using SubtypeCovariance;
+﻿using System;
+using SubtypeCovariance;
 
-class Program
+namespace ConsoleApp
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
 
-        EntityRepository entityRepository = new EntityRepository();
-        Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
+            EntityRepository entityRepository = new EntityRepository();
+            Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
-        myEntity.Name = "Tom";
-        Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
-        Console.WriteLine("New entity Name: " + myEntity.Name);
+            myEntity.Name = "Tom";
+            Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
+            Console.WriteLine("New entity Name: " + myEntity.Name);
 
+        }
     }
 }
